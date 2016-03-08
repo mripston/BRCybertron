d8d1aeba6636bf1df73c2a407b80d3b013dba45a
Matt Magoffin <matt@bluerocket.us>
2016-03-08 20:12:41 +1300

    Add unit test for resolver.

---
+++ BRCybertron/BRCybertronTests/CYSimpleEntityResolverTests.m
@@ -0,0 +1,44 @@
+//
+//  CYSimpleEntityResolverTests.m
+//  BRCybertron
+//
+//  Created by Matt on 8/03/16.
+//  Copyright © 2016 Blue Rocket, Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+#import "CYDataInputSource.h"
+#import "CYParsingContext.h"
+#import "CYSimpleEntityResolver.h"
+
+@interface CYSimpleEntityResolverTests : XCTestCase
+
+@end
+
+@implementation CYSimpleEntityResolverTests
+
+- (void)testResolveInternal {
+	CYSimpleEntityResolver *resolver = [[CYSimpleEntityResolver alloc] init];
+	[resolver addInternalEntities:@{@"foo" : @"bar", @"bim" : @"bam"}];
+	
+	CYDataInputSource *input = [[CYDataInputSource alloc] initWithData:[@"<x/>" dataUsingEncoding:NSUTF8StringEncoding] options:0];
+	CYParsingContext *context = [[CYParsingContext alloc] initWithInputSource:input];
+	
+	id<CYEntity> ent;
+	
+	ent = [resolver resolveEntity:@"foo" context:context];
+	assertThat(ent.name, equalTo(@"foo"));
+	assertThat(ent.content, equalTo(@"bar"));
+	
+	ent = [resolver resolveEntity:@"bim" context:context];
+	assertThat(ent.name, equalTo(@"bim"));
+	assertThat(ent.content, equalTo(@"bam"));
+	
+	ent = [resolver resolveEntity:@"megatron" context:context];
+	assertThat(ent, nilValue());
+}
+
+@end
